1f9a5f97be4a9ca94f4ba707c7d400ec5ac0b803
TotoFabrizio <tomas.fabrizio02@gmail.com>
2021-08-29 11:57:27 -0300

    implemente router y controller, y html a ejs

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,27 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+
 app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
 app.listen(3000, () => {
     console.log("server activo");
 });
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/index.html"));
-});
+const mainRouter = require("./routes/main-router");
 
-app.get("/detail", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/productDetail.html"));
-});
-
-app.get("/cart", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/productCart.html"));
-});
-
-app.get("/register", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/register.html"));
-});
-
-app.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/login.html"));
-});
+app.use("/", mainRouter);
