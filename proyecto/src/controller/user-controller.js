883742dddd7ed7de573ef6560f058a535d7a787a
Juan-Ignacio-Bordignon <juanibordil@gmail.com>
2021-09-10 20:14:14 -0300

    Base de datos(JSON) de usuarios

--- proyecto/src/controller/user-controller.js
+++ proyecto/src/controller/user-controller.js
@@ -1,9 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+
+const pathToUsers = path.join(__dirname,"../data/userDataBase.json");
+const listaUsers = JSON.parse(fs.readFileSync(pathToUsers), "utf-8");
+
+
 const controller = {
     cart: (req, res) => {
         res.render("users/cart");
     },
     register: (req, res) => {
         res.render("users/register");
+    },
+    saveUser:()=>{
+        //guarda el usuario en el JSON/base de datos
     },
     login: (req, res) => {
         res.render("users/login");
--- proyecto/src/routes/user-router.js
+++ proyecto/src/routes/user-router.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controller/user-controller");
+const userController = require("../controller/user-controller");
 
 router.get("/cart", controller.cart);
 
 router.get("/register", controller.register);
+//router.post('/', userController.saveUser); 
 
 router.get("/login", controller.login);
 
