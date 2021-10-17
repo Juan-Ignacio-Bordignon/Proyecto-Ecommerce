ea62308eb28a1c61b2c61907fdf27b51a3204d3f
TotoFabrizio <tomas.fabrizio02@gmail.com>
2021-10-17 18:35:15 -0300

    Mejora sql, instalacion y config sequelize

--- proyecto/src/data/structure.sql
+++ proyecto/src/data/structure.sql
@@ -1,28 +1,23 @@
+DROP DATABASE IF EXISTS `hard4gamers`;
 CREATE DATABASE `hard4gamers`;
 USE `hard4gamers`;
 CREATE TABLE `users`(
-  `id` SMALLINT(6) AUTO_INCREMENT NOT NULL,
+  `id` INT(10) AUTO_INCREMENT NOT NULL,
   `user_name` VARCHAR(50) NOT NULL,
   `email` VARCHAR(50) NOT NULL UNIQUE,
   `password` VARCHAR(60) NOT NULL,
   `category` VARCHAR(50) NOT NULL,
   PRIMARY KEY(id)
 );
-CREATE TABLE `cart_user`(
-  id SMALLINT(6) AUTO_INCREMENT NOT NULL,
-  user_id SMALLINT(6) NOT NULL,
-  PRIMARY KEY(id),
-  FOREIGN KEY (user_id) REFERENCES `users` (`id`)
-);
 CREATE TABLE `types`(
-  id SMALLINT(6) AUTO_INCREMENT NOT NULL,
+  id INT(10) AUTO_INCREMENT NOT NULL,
   `name` VARCHAR(50) NOT NULL,
   PRIMARY KEY(id)
 );
 CREATE TABLE `products`(
-  id SMALLINT(6) AUTO_INCREMENT NOT NULL,
+  id INT(10) AUTO_INCREMENT NOT NULL,
   title VARCHAR(50) NOT NULL,
-  `type` SMALLINT(6) NOT NULL,
+  `type` INT(10) NOT NULL,
   `description` TEXT NOT NULL,
   price DECIMAL NOT NULL,
   `image` VARCHAR(100) NOT NULL,
@@ -31,12 +26,11 @@ CREATE TABLE `products`(
   FOREIGN KEY (`type`) REFERENCES `types` (`id`)
 );
 CREATE TABLE `carts`(
-  id SMALLINT(6) AUTO_INCREMENT NOT NULL,
-  cart_id SMALLINT(6) NOT NULL,
-  product_id SMALLINT(6) NOT NULL,
+  id INT(10) AUTO_INCREMENT NOT NULL,
+  user_id INT(10) NOT NULL,
+  product_id INT(10) NOT NULL,
   cuantity SMALLINT NOT NULL,
-  total_price DECIMAL NOT NULL,
   PRIMARY KEY(id),
   FOREIGN KEY (product_id) REFERENCES `products` (`id`),
-  FOREIGN KEY (cart_id) REFERENCES `cart_user` (`id`)
+  FOREIGN KEY (user_id) REFERENCES `user` (`id`)
 );
--- proyecto/src/routes/product-router.js
+++ proyecto/src/routes/product-router.js
@@ -2,15 +2,16 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controller/product-controller");
 const uploadFile = require("../middlewares/saveFileMiddleware");
+const adminMiddleware = require("../middlewares/adminMiddleware");
 
 //Create and save product
-router.get("/create", controller.creat);
+router.get("/create", adminMiddleware, controller.creat);
 router.post("/create", uploadFile.single("img"), controller.save);
 
 router.get("/detail/:id", controller.detail);
 
 //Edit and update
-router.get("/edit/:id", controller.edit);
+router.get("/edit/:id", adminMiddleware, controller.edit);
 router.patch("/edit/:id", uploadFile.single("img"), controller.update);
 
 //delete
