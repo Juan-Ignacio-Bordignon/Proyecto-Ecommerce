4cc48fcbcd1b6b18ac9c2196d36cfa8a52682669
TotoFabrizio <tomas.fabrizio02@gmail.com>
2021-10-17 19:06:08 -0300

    Corección errores sql

--- proyecto/src/data/structure.sql
+++ proyecto/src/data/structure.sql
@@ -1,36 +1,36 @@
-DROP DATABASE IF EXISTS `hard4gamers`;
-CREATE DATABASE `hard4gamers`;
-USE `hard4gamers`;
-CREATE TABLE `users`(
-  `id` INT(10) AUTO_INCREMENT NOT NULL,
-  `user_name` VARCHAR(50) NOT NULL,
-  `email` VARCHAR(50) NOT NULL UNIQUE,
-  `password` VARCHAR(60) NOT NULL,
-  `category` VARCHAR(50) NOT NULL,
-  PRIMARY KEY(id)
-);
-CREATE TABLE `types`(
+DROP DATABASE IF EXISTS hard4gamers;
+CREATE DATABASE hard4gamers;
+USE hard4gamers;
+CREATE TABLE users(
   id INT(10) AUTO_INCREMENT NOT NULL,
-  `name` VARCHAR(50) NOT NULL,
+  user_name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL UNIQUE,
+  password VARCHAR(60) NOT NULL,
+  category VARCHAR(50) NOT NULL,
   PRIMARY KEY(id)
 );
-CREATE TABLE `products`(
+CREATE TABLE types(
+  id INT(10) AUTO_INCREMENT NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  PRIMARY KEY(id)
+);
+CREATE TABLE products(
   id INT(10) AUTO_INCREMENT NOT NULL,
   title VARCHAR(50) NOT NULL,
-  `type` INT(10) NOT NULL,
-  `description` TEXT NOT NULL,
+  type INT(10) NOT NULL,
+  description TEXT NOT NULL,
   price DECIMAL NOT NULL,
-  `image` VARCHAR(100) NOT NULL,
+  image VARCHAR(100) NOT NULL,
   deleted TINYINT(1) NOT NULL,
   PRIMARY KEY(id),
-  FOREIGN KEY (`type`) REFERENCES `types` (`id`)
+  FOREIGN KEY (type) REFERENCES types (id)
 );
-CREATE TABLE `carts`(
+CREATE TABLE carts(
   id INT(10) AUTO_INCREMENT NOT NULL,
   user_id INT(10) NOT NULL,
   product_id INT(10) NOT NULL,
   cuantity SMALLINT NOT NULL,
   PRIMARY KEY(id),
-  FOREIGN KEY (product_id) REFERENCES `products` (`id`),
-  FOREIGN KEY (user_id) REFERENCES `user` (`id`)
+  FOREIGN KEY (product_id) REFERENCES products (id),
+  FOREIGN KEY (user_id) REFERENCES users (id)
 );
