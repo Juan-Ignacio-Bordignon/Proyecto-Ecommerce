8d9a9d67a84338ac388d1791f828cd0242694d32
Juan-Ignacio-Bordignon <juanibordil@gmail.com>
2021-12-08 01:55:03 -0300

    comentarios

--- proyecto/src/routes/api/api-main-router.js
+++ proyecto/src/routes/api/api-main-router.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const APIproductRouter = require("./api-product-router");
+// const productController = require("../../controller/api/api-product-controller");
 
+//          Users
 //Controler
 let userController = require("../../controller/api/api-users-controller");
-//Users
+//Routes
 router.get("/users", userController.allUsers);
 router.get("/users/:id", userController.userById);
 
-router.use("/product");
-router.use("/product/:id");
+//          Products
+
+/*router.use("/product,    ");
+router.use("/product/:id,   ");*/
 
 module.exports = router;
