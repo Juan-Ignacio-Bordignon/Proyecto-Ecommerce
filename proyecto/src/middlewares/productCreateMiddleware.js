a86ee47cd834843ef56537b1a6ac50134f0980fd
TotoFabrizio <tomas.fabrizio02@gmail.com>
2021-12-15 20:46:47 -0300

    Corección de mayusculas y minusculas

--- proyecto/src/middlewares/productCreateMiddleware.js
+++ proyecto/src/middlewares/productCreateMiddleware.js
@@ -2,14 +2,14 @@ const { check } = require("express-validator");
 
 let validateCreate = [
     check("title")
-        .notEmpty().withMessage("debes incluir el nombre del producto").bail()
+        .notEmpty().withMessage("Debes incluir el nombre del producto").bail()
         .isLength({min:5}).withMessage("El nombre del producto debe tener como minimo 5 caracteres"),
     check("price")
-        .notEmpty().withMessage("debes incluir el precio").bail(),
+        .notEmpty().withMessage("Debes incluir el precio").bail(),
     /*check("img")
         .notEmpty().withMessage("Debes incluir una imagen").bail(),*/
     check("description")
-        .notEmpty().withMessage("debes incluir una descripción").bail()
+        .notEmpty().withMessage("Debes incluir una descripción").bail()
         .isLength({min:20}).withMessage("El nombre del producto debe tener como minimo 5 caracteres"),
 
 ];
--- proyecto/src/middlewares/productUpdateMiddleware.js
+++ proyecto/src/middlewares/productUpdateMiddleware.js
@@ -3,17 +3,17 @@ const { check } = require("express-validator");
 let validateCreate = [
     check("title")
         .notEmpty()
-        .withMessage("debes incluir el nombre del producto")
+        .withMessage("Debes incluir el nombre del producto")
         .bail()
         .isLength({ min: 5 })
         .withMessage(
             "El nombre del producto debe tener como minimo 5 caracteres"
         ),
-    check("price").notEmpty().withMessage("debes incluir el precio").bail(),
+    check("price").notEmpty().withMessage("Debes incluir el precio").bail(),
     check("img"),
     check("description")
         .notEmpty()
-        .withMessage("debes incluir una descripción")
+        .withMessage("Debes incluir una descripción")
         .bail()
         .isLength({ min: 20 })
         .withMessage(
